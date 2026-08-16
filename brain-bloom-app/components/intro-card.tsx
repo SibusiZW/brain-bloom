84940e43836a1459527363ab24f9be2bc2230c01
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-08-16 21:23:42 +0200

    feat: enhance IntroCard component with session handling and routing

--- brain-bloom-app/components/intro-card.tsx
+++ brain-bloom-app/components/intro-card.tsx
@@ -1,13 +1,19 @@
 'use client';
 
+import { authClient } from "@/lib/auth-client";
 import { Button } from "./ui/button";
 import { Brain, LogIn } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function IntroCard() {
+
+    const { data: session } = authClient.useSession();
+    const router = useRouter();
+
     return (
         <div className="w-full max-w-[450px] bg-white rounded-md p-4">
             <h1 className="text-3xl font-serif mb-4">Brain<span className="text-blue-500">Bloom</span></h1>
-            
+            { session ? <Button className={'mb-3 bg-blue-500 w-full'}></Button> : <Button onClick={() => router.push('/auth')} className={'mb-3 bg-blue-500 w-full'}><LogIn /> Log in now!</Button> }
             <p className="text-gray-500">&copy; tafadzwa sibanda {new Date().getFullYear()}</p>
         </div>
     )
