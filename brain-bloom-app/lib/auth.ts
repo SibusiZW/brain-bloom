1ce82c417872528c228c5057657da1a17eba5d05
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-08-16 21:16:14 +0200

    feat: update auth configuration to include schema and social providers

--- brain-bloom-app/lib/auth.ts
+++ brain-bloom-app/lib/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import * as schema from '@/lib/db/schema';
 
 import { db } from "@/lib/db";
 
@@ -14,11 +15,14 @@ if (!process.env.BETTER_AUTH_URL) {
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
-    
+    schema: schema
   }),
   secret: process.env.BETTER_AUTH_SECRET,
   baseURL: process.env.BETTER_AUTH_URL,
-  emailAndPassword: {
-    enabled: true,
-  },
+  socialProviders: {
+    google: {
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    }
+  }
 });
