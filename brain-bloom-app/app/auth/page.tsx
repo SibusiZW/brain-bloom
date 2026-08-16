10577118943731588cb95171b80ccc6391a2ea8d
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-08-16 21:42:08 +0200

    feat: refactor authentication handling in LoginPage and LoginForm components

--- brain-bloom-app/app/auth/page.tsx
+++ brain-bloom-app/app/auth/page.tsx
@@ -1,7 +1,22 @@
+'use client';
+
 import { LoginForm } from "@/components/login-form"
+import { authClient } from "@/lib/auth-client";
 import { Flower } from "lucide-react"
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function LoginPage() {
+
+  const { data: session } = authClient.useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session) {
+      router.push('/dashboard');
+    }
+  }, [session]);
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
--- brain-bloom-app/components/login-form.tsx
+++ brain-bloom-app/components/login-form.tsx
@@ -16,22 +16,28 @@ import {
 } from "@/components/ui/field"
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
+import { toast } from "./ui/toast";
+import { useEffect } from "react";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
 
-  const { data: session } = authClient.useSession();
-  const router = useRouter();
 
-  if (session) {
-    router.push('/');
-    return
-  }
+  async function handleGoogleSignIn() {
+    const { error } = await authClient.signIn.social({
+      provider: 'google',
+      callbackURL: '/dashboard'
+    });
 
-  function handleGoogleSignIn() {
-    
+    if (error) {
+      toast.add({
+        title: error?.message,
+        type: 'error'
+      })
+      return
+    }
   }
 
   return (
@@ -47,7 +53,7 @@ export function LoginForm({
           <form>
             <FieldGroup>
               <Field>
-                <Button className={'bg-blue-500'} type="button">
+                <Button onClick={handleGoogleSignIn} className={'bg-blue-500'} type="button">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                       d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
