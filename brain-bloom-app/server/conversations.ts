815011e9bee03b9d9affc926c6d6ed8f50ce0078
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-08-17 11:13:25 +0200

    feat: add getConversations function to retrieve user-specific conversations

--- brain-bloom-app/server/conversations.ts
+++ brain-bloom-app/server/conversations.ts
@@ -3,6 +3,7 @@
 import { db } from "@/lib/db";
 import { getSession } from "./auth";
 import { conversations } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
 
 export async function createConversation(title: string) {
     const session = await getSession();
@@ -11,5 +12,15 @@ export async function createConversation(title: string) {
     if (userId) {
         const [conservation] = await db.insert(conversations).values({ title: title, userId: userId }).returning();
         return conservation
+    }
+}
+
+export async function getConversations() {
+    const session = await getSession();
+    const userId = session?.user.id;
+
+    if (userId) {
+        const allConversations = await db.select().from(conversations).where(eq(conversations.userId, userId));
+        return allConversations;
     }
 }
