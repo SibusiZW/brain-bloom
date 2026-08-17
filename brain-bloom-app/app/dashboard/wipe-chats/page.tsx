26c94f319013af0cab2880640014674e0b6042d6
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-08-17 12:47:22 +0200

    feat: enhance WipeDialog with loading state and delete confirmation

--- brain-bloom-app/app/dashboard/wipe-chats/page.tsx
+++ brain-bloom-app/app/dashboard/wipe-chats/page.tsx
@@ -2,8 +2,11 @@ import WipeDialog from "@/components/dialogs/wipe-dialog";
 
 export default function WipeChats() {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center">
-            <WipeDialog />
+        <div className="min-h-screen flex flex-col text-center items-center justify-center">
+            <div className="p-4 p-4 w-full max-w-[450px]">
+                <h1 className="font-serif text-3xl mb-4"><span className="text-red-500">Delete</span> Chats</h1>
+                <WipeDialog />
+            </div>
         </div>
     )
 }
--- brain-bloom-app/components/dialogs/wipe-dialog.tsx
+++ brain-bloom-app/components/dialogs/wipe-dialog.tsx
@@ -1,9 +1,44 @@
 'use client';
 
-import { Dialog } from "../ui/dialog";
+import { Loader2, Trash2 } from "lucide-react";
+import { Button } from "../ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogTrigger } from "../ui/dialog";
+import { useState } from "react";
+import { wipeConversations } from "@/server/conversations";
+import { useRouter } from "next/navigation";
 
 export default function WipeDialog() {
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+
+    async function handleDelete() {
+        setLoading(true);
+
+        await wipeConversations();
+
+        setLoading(false);
+        router.push('/dashboard')
+    }
+
     return (
-        <Dialog></Dialog>
+        <Dialog>
+            <DialogTrigger>
+                <Button className={'bg-red-500 hover:bg-red-300'}><Trash2 /> Wipe all conversations</Button>
+            </DialogTrigger>
+
+            <DialogContent className={'text-center'}>
+                <DialogDescription>This deletes all your conversations</DialogDescription>
+
+                <Button onClick={handleDelete} className={'w-full'} variant={'destructive'}>
+                    {
+                        loading ? <Loader2 className="animate-spin"/> : 
+                        <>
+                            <Trash2 />
+                            delete everything :)
+                        </>
+                    }
+                </Button>
+            </DialogContent>
+        </Dialog>
     )
 }
